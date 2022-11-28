1581b907bd2fe0687a8670771c17430b162fc06b
Joel Snowflake <119365443+snowjoel@users.noreply.github.com>
2022-11-28 14:55:08 -0800

    Update streamlit_app.py
    Added pandas, fruit info dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,19 @@
 import streamlit
+import pandas
 
-#This is setting up the text for this app
+#import data
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#App title
 streamlit.title('My Parents New Healthy Diner')
+
+#Menu
 streamlit.header('Breafast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+#BYO Smoothie with info
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
