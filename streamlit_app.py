4cde17c687dcf866214eed913b8742ceeb51ef32
Joel Snowflake <119365443+snowjoel@users.noreply.github.com>
2022-11-29 14:41:50 -0800

    Update streamlit_app.py
    Added a way to select which fruit they want to know more about.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,11 @@ streamlit.dataframe(fruits_to_show)
 
 #Get reponse
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # This reformats data out of Json into something more CSV-like
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
