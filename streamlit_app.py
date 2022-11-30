01d98b90f0c8364dce2416ff2b2c4fd0e7e5ea78
Joel Snowflake <119365443+snowjoel@users.noreply.github.com>
2022-11-30 10:02:34 -0800

    Update streamlit_app.py
    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 #import data
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -38,6 +39,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # This formats fruityvice as a table. 
 streamlit.dataframe(fruityvice_normalized)
+
+#stopping for now - troubleshooting
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
