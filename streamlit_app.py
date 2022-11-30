43dc24690aa2df9cbfdd4266d0d89246d619ab63
Joel Snowflake <119365443+snowjoel@users.noreply.github.com>
2022-11-30 09:46:03 -0800

    Update streamlit_app.py
    bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my.cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hellow from snowflake:")
 streamlit.text(my_data_row)
